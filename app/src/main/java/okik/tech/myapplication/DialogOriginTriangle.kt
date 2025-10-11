be25d979e45ad2299f51e2faeb3219a3e8e89c0b
Juan Gomez <juan.e.g.f@outlook.com>
2025-10-11 00:44:08 -0600

    chore: configure correct attributes

---
+++ app/src/main/java/okik/tech/myapplication/DialogOriginTriangle.kt
@@ -0,0 +1,134 @@
+package okik.tech.myapplication
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.graphics.Path
+import android.view.View
+
+class TutorialDialogContainer @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null,
+) : View(context, attrs) {
+    // This works in combination with TOP or BOTTOM orientation it won't work with LEFT nor RIGHT
+    // orientation
+    private var xOriginOffsetPercent: Float
+    // This works in combination with LEFT or RIGHT orientation, it won't work with TOP nor BOTTOM
+    private var yOriginOffsetPercent: Float
+
+    // this component is intended to be the origin of a dialog, so if set to "top", it will be a triangle upside down
+    // "bottom" is a normal triangle, "left" the triangle is laid to the right(growing from left right to left), "right"
+    // triangle is laid to left(it grows from left to right)
+    private var orientation: String
+
+    private val path: Path
+    private val paint: Paint
+
+    init {
+        val passAttrs = context.obtainStyledAttributes(attrs, R.styleable.TutorialDialogContainer)
+
+        try {
+            yOriginOffsetPercent =
+                passAttrs.getFloat(R.styleable.TutorialDialogContainer_YOriginOffsetPercent, 0.5f)
+
+            xOriginOffsetPercent =
+                passAttrs.getFloat(R.styleable.TutorialDialogContainer_XOriginOffsetPercent, 0.5f)
+
+            orientation =
+                passAttrs.getString(R.styleable.TutorialDialogContainer_Orientation) ?: "bottom"
+
+       } finally {
+            passAttrs.recycle()
+        }
+
+        path = Path()
+
+        paint = Paint()
+        paint.color = Color.WHITE
+        paint.style = Paint.Style.FILL
+        paint.isAntiAlias = true // to render smooth edges
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        path.reset()
+
+        val triangleHeight = height.toFloat()
+        val triangleWidth = width.toFloat()
+
+        var xOrigin: Float
+        var yOrigin: Float
+
+        var xFirstVertex: Float
+        var yFirstVertex: Float
+
+        var xSecondVertex: Float
+        var ySecondVertex: Float
+
+        when(orientation.lowercase()) {
+            "top" -> {
+                xOrigin = triangleWidth * xOriginOffsetPercent
+                yOrigin = triangleHeight
+                xFirstVertex = triangleWidth
+                yFirstVertex = 0f
+                xSecondVertex = 0f
+                ySecondVertex = 0f
+            }
+            "bottom" -> {
+                xOrigin = triangleWidth * xOriginOffsetPercent
+                yOrigin = 0f
+                xFirstVertex = triangleWidth
+                yFirstVertex = triangleHeight
+                xSecondVertex = 0f
+                ySecondVertex = triangleHeight
+            }
+            "right" -> {
+                xOrigin = 0f
+                yOrigin = triangleHeight * yOriginOffsetPercent
+                xFirstVertex = triangleWidth
+                yFirstVertex = 0f
+                xSecondVertex = triangleWidth
+                ySecondVertex = triangleHeight
+            }
+            "left" -> {
+                xOrigin = triangleWidth
+                yOrigin = triangleHeight * yOriginOffsetPercent
+                xFirstVertex = 0f
+                yFirstVertex = 0f
+                xSecondVertex = 0f
+                ySecondVertex = triangleHeight
+            }
+            else -> throw IllegalArgumentException("bad orientation")
+        }
+
+        path.moveTo(xOrigin, yOrigin)
+
+        path.lineTo(xFirstVertex, yFirstVertex)
+        path.lineTo(xSecondVertex, ySecondVertex)
+        path.close()
+
+        canvas.drawPath(path, paint)
+    }
+
+    fun MoveToPosition(position: IntArray, xOffset: Int, yOffset: Int) {
+        translationX = position[0].toFloat() + xOffset
+        translationY = position[1].toFloat() + yOffset
+    }
+
+    fun setDivideByOriginInitX(value: Float) {
+        xOriginOffsetPercent = value
+        invalidate()
+    }
+
+    fun setDivideByOriginInitY(value: Float) {
+        yOriginOffsetPercent = value
+        invalidate()
+    }
+
+    fun setTrianglePosition(value: String) {
+        orientation = value
+        invalidate()
+    }
+}
