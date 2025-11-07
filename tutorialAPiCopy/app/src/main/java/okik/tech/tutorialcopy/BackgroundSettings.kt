be6762ce66917ee0670669a1827da01cd7ca200e
Juan Gomez <juan.e.g.f@outlook.com>
2025-11-07 10:11:15 -0600

    chore: add params description

--- tutorialAPiCopy/app/src/main/java/okik/tech/tutorialcopy/BackgroundSettings.kt
+++ tutorialAPiCopy/app/src/main/java/okik/tech/tutorialcopy/BackgroundSettings.kt
@@ -6,6 +6,14 @@ import android.graphics.RenderEffect
 import android.graphics.drawable.Drawable
 import android.view.View
 
+/**
+ * This class is intended to be used to configure "BackgroundEffectRenderLayout" instances
+ *
+ * @param renderCanvasPositionCommand lets control the location/position of the recording canvas
+ * that copies/draws the views behind a "BackgroundEffectRenderLayout". This callback also passes a
+ * reference pf the BackgroundEffectRenderLayout so you can get things like its location on screen
+ * which is useful when modifying the position of the recording canvas
+ */
 data class BackgroundSettings(
     val renderEffect: RenderEffect?,
     val shouldClipToBackground: Boolean,
