d86281a1c2448798d68ba655b24b18de45918a2e
Juan Gomez <juan.e.g.f@outlook.com>
2025-11-09 23:17:29 -0600

    refactor: display dialog only on views screen area

--- tutorialAPiCopy/app/src/main/java/okik/tech/tutorialcopy/TutorialDisplayLayout.kt
+++ tutorialAPiCopy/app/src/main/java/okik/tech/tutorialcopy/TutorialDisplayLayout.kt
@@ -1,5 +1,6 @@
 package okik.tech.tutorialcopy
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.RenderNode
@@ -13,8 +14,10 @@ import android.widget.PopupWindow
 import androidx.core.view.size
 
 /**
- * This custom layout is expected to only have one child, which is usually a Linear or Constraint
- * Layout or similar.
+ * This custom layout is expected to only have one child, if you add more children they won't render
+ * when displaying a focus view and/or visually attaching a dialog. This layout use a popup window dialog
+ * to disable any interaction with the child view, by default it will be in the same exact position/location
+ * and with thw same width and height as this layout
  */
 class TutorialDisplayLayout @JvmOverloads constructor(
     context: Context,
@@ -34,8 +37,26 @@ class TutorialDisplayLayout @JvmOverloads constructor(
         }
     }
 
+    /**
+     * When you display a dialog using this API you'll be interacting with two components you need to
+     * configure correctly(besides the dialog view you pass in the focus dialog object), you will interact
+     * with an instance of "BackgroundEffectRendererLayout" which serves as the view that lets you add visual
+     * focus on the view you pass in the FocusArea object, on Android 31 and above you have the ability to
+     * add RenderEffects(eg. blur), whether or not you pass a RenderEffect, you have to control the background
+     * views location that are render behind this focus view, since the PopupWindow instance used to render
+     * all this components will have same size and location as the instance of this layout, you have to
+     * make sure the location of the view you're passing is relative to this layout, to get the location of
+     * the view you use apis las "getTop", "getLeft", "View.getLocationOnScreen" however they have different
+     * implications you need to be aware of, nevertheless, your responsibility is to make sure that location
+     * is relative to origin of this layout(beware the plane in android starts at the left top corner and
+     * grows in X axis to the right and grows in Y axis towards bottom", one way to do it right is to
+     * get location on screen and then subtract top status bar insets and top bar(if any) if portrait orientation and
+     * bottom navigation bar left inset and top bar(if any) if landscape orientation, as long as you calculate
+     * this correctly, the views behind focus area will be render correctly, it really depends on where in the
+     * view hierarchy you have added this layout to. Usually you'd want to add it as the root of you app(in
+     * your main activity layout)
+     */
     fun renderFocusAreaWithDialog(focusArea: FocusArea, focusDialog: FocusDialog) {
-//        this.renderFocusArea(focusArea)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             // this custom layout should only have one child and we record its content with no effect here
             // so we can make a copy of the area we want to focus on
@@ -56,10 +77,16 @@ class TutorialDisplayLayout @JvmOverloads constructor(
             contentCopy
         )
 
+        val loc = IntArray(2)
+        getLocationOnScreen(loc)
+
+        val possibleWidth = resources.displayMetrics.widthPixels - loc[0]
+        val possibleHeight = resources.displayMetrics.heightPixels - loc[1]
+
         popup = PopupWindow(
             dialogWrapperLayout,
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT,
+            width.coerceAtMost(possibleWidth),
+            height.coerceAtMost(possibleHeight),
             true // closes on outside touche if true
         )
 
@@ -70,7 +97,7 @@ class TutorialDisplayLayout @JvmOverloads constructor(
 
         popup!!.setOnDismissListener(PopupWindow.OnDismissListener { popup = null })
 
-        popup!!.showAtLocation(this, Gravity.NO_GRAVITY, 0, 0)
+        popup!!.showAtLocation(this, Gravity.NO_GRAVITY, loc[0], loc[1])
     }
 
     fun hideTutorialComponents() {
