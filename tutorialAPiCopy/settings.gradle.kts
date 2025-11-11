386c1c8cb69d612074196d9299f9de5ebd9e85e9
Juan Gomez <juan.e.g.f@outlook.com>
2025-11-11 14:40:39 -0600

    chore: add local repo and dependency

--- tutorialAPiCopy/settings.gradle.kts
+++ tutorialAPiCopy/settings.gradle.kts
@@ -16,9 +16,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        mavenLocal()
     }
 }
 
 rootProject.name = "TutorialCopy"
 include(":app")
- 
+include(":coachmark")
